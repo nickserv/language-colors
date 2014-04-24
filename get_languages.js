73816a36d1441c47f271800e56366a2170d5a60a
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-23 23:30:38 -0400

    Call JSON.stringify in the view

--- get_languages.js
+++ get_languages.js
@@ -5,7 +5,7 @@ module.exports = function (callback) {
   var languagesURL = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml';
 
   request(languagesURL, function (error, response, body) {
-    var languages = JSON.stringify(yaml.safeLoad(body), null, '    ');
+    var languages = yaml.safeLoad(body);
 
     if (!error && response.statusCode === 200) {
       callback(languages);
