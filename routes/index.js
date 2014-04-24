bfeb495b4431ca76eaad3db65c217f77c9b71fba
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-24 03:34:07 -0400

    Minor reformat of index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 var languageColors = require('../language_colors');
 
-/* GET home page. */
+// GET home page
 languageColors(function (languages) {
+  // HTML
   router.get('/', function(req, res) {
-    res.render('index', { title: 'Language Colors', languages: languages });
+    res.render('index', {
+      title: 'Language Colors',
+      languages: languages
+    });
   });
+
+  // JSON
   router.get('/index.json', function(req, res) {
     res.json(languages);
   });
