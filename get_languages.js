22dcfabcf65bdbc2a4b3f30f7369e8a9886cfa41
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-23 23:22:14 -0400

    Convert the language data from YAML to JSON (and then a String for now)

--- get_languages.js
+++ get_languages.js
@@ -1,11 +1,14 @@
 var request = require('request');
+var yaml = require('js-yaml');
 
 module.exports = function (callback) {
   var languagesURL = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml';
 
   request(languagesURL, function (error, response, body) {
+    var languages = JSON.stringify(yaml.safeLoad(body), null, '    ');
+
     if (!error && response.statusCode === 200) {
-      callback(body);
+      callback(languages);
     } else {
       callback(undefined);
     }
