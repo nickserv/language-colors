104628fd6ad63d247b66a7d96e5bb751847b3507
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-24 06:20:17 -0400

    Add doc comments

--- language_colors.js
+++ language_colors.js
@@ -1,8 +1,12 @@
 var https = require('https');
 var yaml = require('js-yaml');
 
+// The URL of the data file with GitHub's language colors.
 var languagesURL = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml';
 
+// Given an Object of languages (from language name Strings to Objects),
+// filterColors will return an Object of language name Strings to String hex
+// color values, with all color-less languages removed.
 function filterColors(languages) {
   return Object.keys(languages).reduce(function (memo, languageName) {
     var color = languages[languageName].color;
@@ -14,6 +18,10 @@ function filterColors(languages) {
   }, {});
 }
 
+// languageColors accepts a callback, downloads GitHub's language colors,
+// creates a new languages Object, and passes it to the callback when it is
+// ready. The languages Object has String language name keys and String hex
+// color values (with leading "#"s).
 module.exports = function (callback) {
   var body = '';
 
