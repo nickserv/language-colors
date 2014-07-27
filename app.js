414e394efbcbc74d7ac3313cbb984db8197a493e
Nicolas McCurdy <thenickperson@gmail.com>
2014-07-27 05:49:33 -0400

    Update generated Express code

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-/// catch 404 and forwarding to error handler
+/// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
