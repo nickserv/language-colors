78c918ce55dc50079fe59a62e45b3bab18e5aa4e
Nicolas McCurdy <thenickperson@gmail.com>
2014-08-01 15:24:22 -0400

    Remove unused dependencies

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var autoprefixer = require('express-autoprefixer');
 
 var routes = require('./routes/index');
@@ -16,9 +14,6 @@ app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(autoprefixer());
 app.use(express.static(path.join(__dirname, 'public')));
 
